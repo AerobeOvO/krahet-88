0163300028a36ea53e592a2d0b6202cc6186778c
Yanjun Shu <yanjun.shu@skyworksinc.com>
2025-09-09 17:17:06 +0800

    new qns

---
+++ Double Pointer/reverseSentence.c
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s= s.strip()
+        j = len(s)-1
+        i =j
+        word =[]
+
+        while i >= 0:
+            while i >= 0 and s[i] != ' ': i -= 1
+            word.append(s[i + 1: j + 1])         
+            while i >= 0 and s[i] == ' ': i -= 1 
+            j = i                       
+        return ' '.join(word)  
